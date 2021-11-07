5fb994a6007930d787c7ef922473a48f4a8c7574
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-11-07 09:07:09 -0600

    deleting movie works

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,10 @@ const App = (props) => {
       });
   }, []);
 
-  const deleteMovie = (id)=> {
+  const deleteMovie = (id) => {
+    setMovies(movies.filter(item => {
+      return item.id !== id
+    }))
   }
 
   const addToFavorites = (movie) => {
@@ -49,7 +52,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
